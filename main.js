502a41815f217b05429d334616a462037a294c09
Gary Grant <garygrant@garys-air.myfiosgateway.com>
2020-11-15 18:37:22 -0500

    success with api

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-//fox news https://saurav.tech/NewsAPI/everything/fox-news.json
-//cnn news https://saurav.tech/NewsAPI/everything/cnn.json
-//bbc news https://saurav.tech/NewsAPI/everything/bbc-news.json
-//top headlines france https://saurav.tech/NewsAPI/top-headlines/category/business/fr.json
-//top headlines usa https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
-//top headlines uk https://saurav.tech/NewsAPI/top-headlines/category/business/gb.json
-//top headlines russia https://saurav.tech/NewsAPI/top-headlines/category/business/ru.json
 
-//first api
-// const url = "https://saurav.tech/NewsAPI/everything/cnn.json"
-const test=async ()=>{
+const newsReports=async ()=>{
     const bccUrl = 'https://saurav.tech/NewsAPI/everything/bbc-news.json'
     const cnnUrl = 'https://saurav.tech/NewsAPI/everything/cnn.json'
     const foxUrl = 'https://saurav.tech/NewsAPI/everything/fox-news.json'
@@ -22,18 +13,57 @@ const test=async ()=>{
         let topUsNews = await axios.get(topNewsUs)
         let topUkNews = await axios.get(topNewsUk)
         let topRuNews = await axios.get(topNewsRu)
-        console.log(topUsNews)
-        console.log(topUkNews)
-        console.log(topRuNews)
-        console.log(bccResponse)
-        console.log(cnnResponse)
-        console.log(foxResponse)
+        console.log(bccResponse.data.articles[0])
+        // console.log(cnnResponse.data.articles[0].title)
+        // console.log(foxResponse.data.articles[0].title)
+        // console.log(topUsNews.data.articles[0].title)
+        // console.log(topUkNews.data.articles[1].title)
+        let bbcList = bccResponse.data.articles[0]
+        let cnnList = cnnResponse.data.articles[0]
+        let foxList = foxResponse.data.articles[0]
+        let usList = topUsNews.data.articles[0]
+        let ukList = topUkNews.data.articles[0]
+        let ruList = topRuNews.data.articles[0]
+        bccLoop(bbcList)
+        cnnLoop(cnnList)
+        foxLoop(foxList)
+        usLoop(usList)
+        ukLoop(ukList)
+        ruLoop(ruList)
     } catch (error) {
         
     }
 }
 
-test()
+newsReports()
+
+function bccLoop(bbcList){
+    console.log(bbcList.source.name)
+    console.log(bbcList.title)
+    console.log(bbcList.url)
+    console.log(bbcList.publishedAt)
+    let bccData = `
+   <img src="${bbcList.urlToImage}" height=500px width=100%>
+    `;
+    let bbcDiv= document.querySelector('#flex-box1')
+    bbcDiv.insertAdjacentHTML('afterbegin',bccData )
+}
+
+function cnnLoop(cnnList){
+   console.log(cnnList) 
+}
+function foxLoop(foxList){
+    console.log(foxList)   
+}
+function usLoop(usList){
+    console.log(usList)
+}
+function ukLoop(ukList){
+  console.log(ukList)
+}
+function ruLoop(ruList){
+   console.log(ruList)
+}
 
 
 
