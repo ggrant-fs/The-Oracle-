33c402adec00f78c548cacb1ef67c63c0ecf437c
Gary Grant <garygrant@Garys-MacBook-Air.local>
2020-11-17 02:07:46 -0500

    fucntion pic1 works

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 
 const newsReports=async ()=>{
     const url = 'https://saurav.tech/NewsAPI/everything/bbc-news.json'
+    const url2 ='https://saurav.tech/NewsAPI/top-headlines/category/business/us.json'
     try{
         let bbcApi = await axios.get(url)
         let apiResponse = bbcApi.data.articles[0]
+        let usNewsApi = await axios.get(url2)
+        let usNews = usNewsApi.data.articles[0]
+        // console.log(usNews)
         mainImg(apiResponse)
+        pic1(usNews)
 
     }catch(error){
 
@@ -17,4 +22,14 @@ function mainImg(apiResponse){
   let mainPic = document.querySelector('#featured-article')
   mainPic.style.backgroundImage = `url("${apiResponse.urlToImage}")`
   
+}
+
+function pic1(usNews){
+ console.log(usNews)
+ let newsPic1 = document.querySelector('#sub-block1')
+ newsPic1.style.backgroundImage = `url("${usNews.urlToImage}")`
+}
+
+function pic2(){
+
 }
